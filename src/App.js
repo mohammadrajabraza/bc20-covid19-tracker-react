6853bd8f71c6b318dafbb86bd862f3e224bf1c78
Rajab Raza <mohammadrajabraza@gmail.com>
2020-06-25 18:29:14 +0500

    brought created components to the app view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Cards  from './components/Cards/Cards';
+import Chart  from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import styles from './App.module.css';
+
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Covid Tracker App</h1>
+            <div className={ styles.container}>
+                <Cards />
+                <Chart />
+                <CountryPicker />
             </div>
         )
     }
